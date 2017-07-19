582ec9c9ecb75a172c968f955e2555be76ec0534
Michael A. Agarenzo <magarenzo@gmail.com>
2017-07-19 11:57:59 -0400

    Update bot.py
    Made the code specific to what my bot does and needs

--- bot.py
+++ bot.py
@@ -1,51 +1,33 @@
-# Copyright (c) 2015–2016 Molly White
-#
-# Permission is hereby granted, free of charge, to any person obtaining a copy
-# of this software and associated documentation files (the "Software"), to deal
-# in the Software without restriction, including without limitation the rights
-# to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
-# copies of the Software, and to permit persons to whom the Software is
-# furnished to do so, subject to the following conditions:
-#
-# The above copyright notice and this permission notice shall be included in
-# all copies or substantial portions of the Software.
-#
-# THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
-# IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
-# FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
-# AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
-# LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
-# OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE
-# SOFTWARE.
-
 import os
 import tweepy
 from secrets import *
 from time import gmtime, strftime
 
-
-# ====== Individual bot configuration ==========================
-bot_username = ''
+bot_username = 'fli_birdz_bot'
 logfile_name = bot_username + ".log"
 
-# ==============================================================
-
-
+# read first line from file, delete that line from the file, tweet the line
 def create_tweet():
-    """Create the text of the tweet you want to send."""
-    # Replace this with your code!
-    text = ""
+
+    f = open("./facts.txt", "r")
+    text = f.readline()
+    if text == "":
+        text = "I need to be given more facts to tweet!"
+    lines = f.readlines()
+    f.close()
+    f = open("./facts.txt", "w")
+    for line in lines:
+        if line != text + "\n":
+            f.write(line)
     return text
 
-
+# authenticate connection to twitter account and send the tweet
 def tweet(text):
-    """Send out the text as a tweet."""
-    # Twitter authentication
+
     auth = tweepy.OAuthHandler(C_KEY, C_SECRET)
     auth.set_access_token(A_TOKEN, A_TOKEN_SECRET)
     api = tweepy.API(auth)
 
-    # Send the tweet and log success or failure
     try:
         api.update_status(text)
     except tweepy.error.TweepError as e:
@@ -53,14 +35,13 @@ def tweet(text):
     else:
         log("Tweeted: " + text)
 
-
+# write to the log what occurred when trying to authenticate / send the tweet
 def log(message):
-    """Log message to logfile."""
+
     path = os.path.realpath(os.path.join(os.getcwd(), os.path.dirname(__file__)))
     with open(os.path.join(path, logfile_name), 'a+') as f:
         t = strftime("%d %b %Y %H:%M:%S", gmtime())
-        f.write("\n" + t + " " + message)
-
+        f.write("\n" + t + " " + str(message))
 
 if __name__ == "__main__":
     tweet_text = create_tweet()
