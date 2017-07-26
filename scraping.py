df5fbd862f69357ae816d426816b958ed864f85c
Michael A. Agarenzo <20476166+magarenzo@users.noreply.github.com>
2017-07-26 11:35:18 -0400

    Add scraping.py
    Add script which scraped four chosen websites for its bird facts and added them to the file bot.py pulls facts to tweet from

---
+++ scraping.py
@@ -0,0 +1,95 @@
+from BeautifulSoup import BeautifulSoup
+import requests
+
+# TODO: remove repition using recursion
+
+# 46 tweets below | start date: 7/26/17 | end date: 9/20
+
+# 11 tweets
+url = 'http://www.sciencekids.co.nz/sciencefacts/animals/bird.html'
+page = requests.get(url)
+c = page.content
+soup = BeautifulSoup(c)
+
+ul = soup.find('ul', {'class': 'style33'})
+children = ul.findChildren('p')
+f = open('facts.txt', 'w')
+for child in children:
+	line = child.text
+	fact = line.encode('ascii', 'ignore')
+	count = len(fact)
+	if (count <= 140):
+		f.write(fact + "\n")
+f.close()
+
+# Store facts already written from the first scrape
+f = open('facts.txt', 'r')
+lines = f.readlines()
+f.close()
+
+# 12 tweets
+url = 'https://www.mspca.org/pet_resources/interesting-facts-about-birds/'
+page = requests.get(url)
+c = page.content
+soup = BeautifulSoup(c)
+
+div = soup.find('div', {'class': 'leftColumn'})
+children = div.findChildren('p')
+f = open('facts.txt', 'w')
+for line in lines:
+  f.write(line)
+for child in children:
+  line = child.text
+  fact = line.encode('ascii', 'ignore')
+  count = len(fact)
+  if (count <= 140):
+    f.write(fact + "\n")
+f.close()
+
+# Store facts already written from the first scrape and second scrapes
+f = open('facts.txt', 'r')
+lines = f.readlines()
+f.close()
+
+# 12 tweets
+url = 'http://www.peteducation.com/article.cfm?c=15+1794&aid=179'
+page = requests.get(url)
+c = page.content
+soup = BeautifulSoup(c)
+
+div = soup.find('div', {'class': 'artext'})
+children = div.findChildren('p')
+f = open('facts.txt', 'w')
+for line in lines:
+  f.write(line)
+for child in children:
+  line = child.text
+  fact = line.encode('ascii', 'ignore')
+  count = len(fact)
+  if (count <= 140):
+    f.write(fact + "\n")
+f.close()
+
+# Store facts already written from the first, second and third scrapes
+f = open('facts.txt', 'r')
+lines = f.readlines()
+f.close()
+
+# 11 tweets
+url = 'https://kidskonnect.com/animals/bird/'
+page = requests.get(url)
+c = page.content
+soup = BeautifulSoup(c)
+
+div = soup.find('div', {'class': 'entry-content'})
+children = div.findChildren('li')
+f = open('facts.txt', 'w')
+for line in lines:
+  f.write(line)
+for child in children:
+  line = child.text
+  fact = line.encode('ascii', 'ignore')
+  count = len(fact)
+  if (count <= 140):
+    f.write(fact + "\n")
+f.close()
