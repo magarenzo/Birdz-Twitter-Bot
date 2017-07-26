0329dfcedbaffb848ad70e816ae6713ee1869ce7
Michael A. Agarenzo <20476166+magarenzo@users.noreply.github.com>
2017-07-26 11:44:29 -0400

    Update bot.py

--- bot.py
+++ bot.py
@@ -1,27 +1,33 @@
-import os
-import tweepy
+#!/usr/bin/env python
+
+import os, tweepy
 from secrets import *
 from time import gmtime, strftime
 
-bot_username = 'fli_birdz_bot'
+bot_username = "fli_birdz_bot"
 logfile_name = bot_username + ".log"
 
-# read first line from file, delete that line from the file, tweet the line
+# Read first line from file and delete it
 def create_tweet():
 
-    f = open("./facts.txt", "r")
-    text = f.readline()
-    if text == "":
-        text = "I need to be given more facts to tweet!"
-    lines = f.readlines()
-    f.close()
-    f = open("./facts.txt", "w")
-    for line in lines:
-        if line != text + "\n":
-            f.write(line)
-    return text
+    facts = 'C:\facts.txt'
 
-# authenticate connection to twitter account and send the tweet
+    f = open(facts, 'r')
+    text = f.readline()
+    f.close()
+    if text == "":
+        text = "I need to be fed more facts to tweet!"
+    else:
+        f = open(facts, 'r')
+        lines = f.readlines()
+        f.close()
+        f = open(facts, 'w')
+        for line in lines:
+            if line != (text):
+                f.write(line)
+        return text
+
+# Authenticate connection to twitter account and send the tweet
 def tweet(text):
 
     auth = tweepy.OAuthHandler(C_KEY, C_SECRET)
@@ -35,14 +41,14 @@ def tweet(text):
     else:
         log("Tweeted: " + text)
 
-# write to the log what occurred when trying to authenticate / send the tweet
+# Write to the log what occurred when trying to authenticate / send the tweet
 def log(message):
 
     path = os.path.realpath(os.path.join(os.getcwd(), os.path.dirname(__file__)))
     with open(os.path.join(path, logfile_name), 'a+') as f:
-        t = strftime("%d %b %Y %H:%M:%S", gmtime())
+        t = strftime('%d %b %Y %H:%M:%S', gmtime())
         f.write("\n" + t + " " + str(message))
 
-if __name__ == "__main__":
+if __name__ == '__main__':
     tweet_text = create_tweet()
     tweet(tweet_text)
