6056980b82919e0dec01860374c7f706884f448c
Michael A. Agarenzo <20476166+magarenzo@users.noreply.github.com>
2017-08-03 18:45:01 -0400

    added follow()

--- follow-back.py
+++ follow-back.py
@@ -13,14 +13,19 @@ auth.set_access_token(A_TOKEN, A_TOKEN_SECRET)
 api = tweepy.API(auth)
 
 # Follow back followers
-for follower in tweepy.Cursor(api.followers).items():
-  follower.follow()
-  log("Followed: " + follower.screen_name)
-  
-  # Write to the log what occurred when trying to authenticate / send the tweet
+def follow():
+
+  for follower in tweepy.Cursor(api.followers).items():
+    follower.follow()
+    log("Followed: " + follower.screen_name)
+
+# Write to the log what occurred when trying to authenticate / send the tweet
 def log(message):
 
     path = os.path.realpath(os.path.join(os.getcwd(), os.path.dirname(__file__)))
     with open(os.path.join(path, logfile_name), 'a+') as f:
         t = strftime('%d %b %Y %H:%M:%S', gmtime())
         f.write("\n" + t + " " + str(message))
+
+if __name__ == '__main__':
+  follow()
