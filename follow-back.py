48325d77a6ba5b783a2ba324e643bbe1b452d7c1
Michael A. Agarenzo <michaelagarenzo@mail.adelphi.edu>
2017-08-15 10:50:45 -0400

    fixed indentation

--- follow-back.py
+++ follow-back.py
@@ -13,18 +13,18 @@ api = tweepy.API(auth)
 
 # Follow back followers
 def follow():
-  for follower in tweepy.Cursor(api.followers).items():
-    follower.follow()
-    log("Followed: " + follower.screen_name)
+        for follower in tweepy.Cursor(api.followers).items():
+                follower.follow()
+                log("Followed: " + follower.screen_name)
 
 # Write to the log what occurred when trying to authenticate / send the tweet
 def log(message):
-  path = os.path.realpath(os.path.join(os.getcwd(), os.path.dirname(__file__)))
-  with open(os.path.join(path, logfile_name), 'a+') as f:
-    t = strftime('%d %b %Y %H:%M:%S', gmtime())
-    f.write("\n" + t + " " + str(message))
+        path = os.path.realpath(os.path.join(os.getcwd(), os.path.dirname(__file__)))
+        with open(os.path.join(path, logfile_name), 'a+') as f:
+                t = strftime('%d %b %Y %H:%M:%S', gmtime())
+                f.write("\n" + t + " " + str(message))
 
 def main():
-  follow()
+        follow()
 
 main()
