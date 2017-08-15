4847e5f826586d4e3da3ee7e3e563c7483ad615b
Michael A. Agarenzo <michaelagarenzo@mail.adelphi.edu>
2017-08-15 10:48:01 -0400

    fixed indentation

--- bot.py
+++ bot.py
@@ -8,45 +8,45 @@ logfile_name = "fli_birdz_bot.log"
 
 # Read first line from file and delete it
 def create_tweet():
-  facts = './facts.txt'
+        facts = '/home/mike/fli-birdz-twitter-bot-master/facts.txt'
 
-  f = open(facts, 'r')
-  text = f.readline()
-  f.close()
-  if text == "":
-    text = "I need to be fed more facts to tweet!"
-  else:
-    f = open(facts, 'r')
-    lines = f.readlines()
-    f.close()
-    f = open(facts, 'w')
-    for line in lines:
-      if line != (text):
-        f.write(line)
-    return text
+        f = open(facts, 'r')
+        text = f.readline()
+        f.close()
+        if text == "":
+                text = "I need to be fed more facts to tweet!"
+        else:
+                f = open(facts, 'r')
+                lines = f.readlines()
+                f.close()
+                f = open(facts, 'w')
+                for line in lines:
+                        if line != (text):
+                        f.write(line)
+                return text
 
 # Authenticate connection to twitter account and send the tweet
 def tweet(text):
-  auth = tweepy.OAuthHandler(C_KEY, C_SECRET)
-  auth.set_access_token(A_TOKEN, A_TOKEN_SECRET)
-  api = tweepy.API(auth)
+        auth = tweepy.OAuthHandler(C_KEY, C_SECRET)
+        auth.set_access_token(A_TOKEN, A_TOKEN_SECRET)
+        api = tweepy.API(auth)
 
-  try:
-    api.update_status(text)
-  except tweepy.error.TweepError as e:
-      log(e.message)
-  else:
-    log("Tweeted: " + text)
+        try:
+                api.update_status(text)
+        except tweepy.error.TweepError as e:
+                log(e.message)
+        else:
+                log("Tweeted: " + text)
 
 # Write to the log what occurred when trying to authenticate / send the tweet
 def log(message):
-  path = os.path.realpath(os.path.join(os.getcwd(), os.path.dirname(__file__)))
-  with open(os.path.join(path, logfile_name), 'a+') as f:
-    t = strftime('%d %b %Y %H:%M:%S', gmtime())
-    f.write("\n" + t + " " + str(message))
+        path = os.path.realpath(os.path.join(os.getcwd(), os.path.dirname(__file__)))
+        with open(os.path.join(path, logfile_name), 'a+') as f:
+                t = strftime('%d %b %Y %H:%M:%S', gmtime())
+                f.write("\n" + t + " " + str(message))
 
 def main():
-  tweet_text = create_tweet()
-  tweet(tweet_text)
+        tweet_text = create_tweet()
+        tweet(tweet_text)
 
 main()
