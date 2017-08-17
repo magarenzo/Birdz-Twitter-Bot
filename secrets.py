3a055a493c1c334b2bffc40d0812a52a88901b27
Michael A. Agarenzo <michaelagarenzo@mail.adelphi.edu>
2017-08-17 11:47:30 -0400

    Update secrets.py

--- secrets.py
+++ secrets.py
@@ -1,4 +1,4 @@
-C_KEY = ""
-C_SECRET = ""
-A_TOKEN = ""
-A_TOKEN_SECRET = ""
+C_KEY = "secret_string_1"
+C_SECRET = "secret_string_2"
+A_TOKEN = "secret_string_3"
+A_TOKEN_SECRET = "secret_string_4"
